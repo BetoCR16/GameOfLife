5f765975af59d76fb2148834134df51802a08891
Roberto Méndez <48104122+BetoCR16@users.noreply.github.com>
2019-07-16 20:18:00 -0600

    Final

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+
 function make2DArray(cols, rows){
     let arr = new Array(cols);
     for(let i = 0; i < arr.length; i++){
@@ -6,11 +11,6 @@ function make2DArray(cols, rows){
     }
     return arr;
 }
-
-let grid;
-let cols;
-let rows;
-let resolution = 20;
 
 function setup(){
     createCanvas(400,400);
@@ -35,16 +35,41 @@ function draw(){
                 fill(255);
                 rect (x,y,resolution,resolution);  
             }
-            
         }
     }
     
     let next = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++){
         for(let j = 0; j < rows; j++){
-            
+            let state = grid[i][j];
+           
+            //Count neighbors
+            let sum = 0;
+            let neighbors = countNeighbors(grid, i, j);
+
+            if (state == 0 && neighbors == 3){
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 ||  neighbors > 3)){
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+            }
         }
     }
-
     grid = next;
+}
+
+function countNeighbors(grid, x, y){
+    let sum = 0;
+    for (let i = -1; i < 2; i++){
+        for (let j = -1; j < 2; j++){
+
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;  
 }
